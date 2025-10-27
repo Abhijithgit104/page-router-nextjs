a5245f8fc2af6dcaa8e71c89cdaeeb101c633bd7
Abhijith T M <abhijithtm861@gmail.com>
2025-10-27 21:17:43 +0530

    added

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,22 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  message: string
-  timestamp: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data>,
 ) {
-
-  if (req.method === 'GET') {
-    res.status(200).json({
-      message: '👋 Hello from Next.js API Route!',
-      timestamp: new Date().toISOString(),
-    })
-  } else {
-    res.setHeader('Allow', ['GET'])
-    res.status(405).end(`Method ${req.method} Not Allowed`)
-  }
+  res.status(200).json({ name: "John Doe" });
 }
